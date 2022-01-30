0feac95fae5378cbdb6eca7a6e020e18289b5ab8
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-01-30 22:01:14 +0500

    Contact Section Completed

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,5 +1,30 @@
 import "./testimonials.scss";
 
 export default function Testimonials() {
-  return <div className="testimonials" id="testimonials"></div>;
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        <div className="card">
+          <div className="top">
+            <img src="assets/right-arrow.png" className="left" alt="" />
+            <img
+              className="user"
+              src="https://images.assetsdelivery.com/compings_v2/luismolinero/luismolinero1909/luismolinero190917934.jpg"
+              alt=""
+            />
+            <img className="right" src="assets/youtube.png" alt="" />
+          </div>
+          <div className="center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            veritatis est quos itaque omnis.
+          </div>
+          <div className="bottom">
+            <h3>Alex</h3>
+            <h4>CEO of BrightSphere</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
