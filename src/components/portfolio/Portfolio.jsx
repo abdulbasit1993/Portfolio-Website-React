2aa4e0a1739806e699540867a1e63cc0a7d3f8ca
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-01-30 18:07:47 +0500

    Portfolio Section Completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,22 +1,62 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import { featuredPortfolio, webPortfolio, mobilePortfolio } from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+  ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
+
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul>
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Mobile App</li>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img
-            src="https://assets.materialup.com/uploads/cfe42246-e49f-4166-91ce-246e253eae27/preview.jpg"
-            alt=""
-          />
-          <h3>Weather App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img src={d.img} alt="" />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "https://cdn.dribbble.com/users/1997192/screenshots/15468493/media/9a11fc9634df26b59d584bd39fd2c6ed.png?compress=1&resize=350x260",
+  },
+  {
+    id: 2,
+    title: "E-Commerce Web App",
+    img: "https://cdn.dribbble.com/users/1715186/screenshots/14976041/media/0f8addb8098436e94318bea48d1f9d01.png?compress=1&resize=350x260",
+  },
+  {
+    id: 3,
+    title: "Banking App",
+    img: "https://cdn.dribbble.com/users/4189231/screenshots/13547910/media/e3acf24a56b629100a4de1e81921be61.jpg?compress=1&resize=350x260",
+  },
+  {
+    id: 4,
+    title: "Weather App",
+    img: "https://cdn.dribbble.com/users/702789/screenshots/15342976/weather_app_16.03_dribbble.png?compress=1&resize=350x260",
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Web Social Media App",
+    img: "https://cdn.dribbble.com/users/78806/screenshots/15327109/media/30fd93b62aa520792bfb4956f7eed854.png?compress=1&resize=350x260",
+  },
+  {
+    id: 2,
+    title: "Web E-Commerce App",
+    img: "https://cdn.dribbble.com/users/1715186/screenshots/14976041/media/0f8addb8098436e94318bea48d1f9d01.png?compress=1&resize=350x260",
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Mobile Social Media App",
+    img: "https://cdn.dribbble.com/users/1997192/screenshots/15468493/media/9a11fc9634df26b59d584bd39fd2c6ed.png?compress=1&resize=350x260",
+  },
+  {
+    id: 2,
+    title: "Mobile Weather App",
+    img: "https://cdn.dribbble.com/users/702789/screenshots/15342976/weather_app_16.03_dribbble.png?compress=1&resize=350x260",
+  },
+  {
+    id: 3,
+    title: "Mobile Banking App",
+    img: "https://cdn.dribbble.com/users/4189231/screenshots/13547910/media/e3acf24a56b629100a4de1e81921be61.jpg?compress=1&resize=350x260",
+  },
+];
