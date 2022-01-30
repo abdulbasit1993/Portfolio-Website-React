e6543d021e9083e873a882d6c9261add42855098
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-01-30 15:58:19 +0500

    Creating Portfolio Section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,23 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://assets.materialup.com/uploads/cfe42246-e49f-4166-91ce-246e253eae27/preview.jpg"
+            alt=""
+          />
+          <h3>Weather App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
