538b4f8acd686f526e8cd75ce96598dcf060b1a2
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-10-03 00:55:24 +0500

    Added a web portfolio project

--- src/data.js
+++ src/data.js
@@ -1,15 +1,22 @@
-import CalculatorWebApp_IMG from './assets/images/Calculator-WebApp.png';
-import ComingSoon_IMG from './assets/images/coming-soon.jpg';
+import CalculatorWebApp_IMG from "./assets/images/Calculator-WebApp.png";
+import TodoWebApp_IMG from "./assets/images/images/todo-web-app.png";
+import ComingSoon_IMG from "./assets/images/coming-soon.jpg";
 
 export const featuredPortfolio = [
   {
     id: 1,
-    title: "Calculator Web App",
-    img: CalculatorWebApp_IMG,
-    link: "https://abdulbasit1993.github.io/Calculator-WebApp/"
+    title: "Todo Web App MERN",
+    img: TodoWebApp_IMG,
+    link: "https://mern-todo-web-app.vercel.app/",
   },
   {
     id: 2,
+    title: "Calculator Web App",
+    img: CalculatorWebApp_IMG,
+    link: "https://abdulbasit1993.github.io/Calculator-WebApp/",
+  },
+  {
+    id: 3,
     title: "Coming Soon",
     img: ComingSoon_IMG,
   },
@@ -18,12 +25,18 @@ export const featuredPortfolio = [
 export const webPortfolio = [
   {
     id: 1,
-    title: "Calculator Web App",
-    img: CalculatorWebApp_IMG,
-    link: "https://abdulbasit1993.github.io/Calculator-WebApp/"
+    title: "Todo Web App MERN",
+    img: TodoWebApp_IMG,
+    link: "https://mern-todo-web-app.vercel.app/",
   },
   {
     id: 2,
+    title: "Calculator Web App",
+    img: CalculatorWebApp_IMG,
+    link: "https://abdulbasit1993.github.io/Calculator-WebApp/",
+  },
+  {
+    id: 3,
     title: "Coming Soon",
     img: ComingSoon_IMG,
   },
