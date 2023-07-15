0d314bd98698d17d72db2af43783ec33aaee082f
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2023-07-15 18:54:42 +0500

    Added link to Pakfit App

--- src/data.js
+++ src/data.js
@@ -1,22 +1,29 @@
 import CalculatorWebApp_IMG from "./assets/images/Calculator-WebApp.png";
 import TodoWebApp_IMG from "./assets/images/todo-list-web-app.png";
 import ComingSoon_IMG from "./assets/images/coming-soon.jpg";
+import PakFit_IMG from "./assets/images/pakfit.png";
 
 export const featuredPortfolio = [
   {
     id: 1,
+    title: "PakFit",
+    img: PakFit_IMG,
+    link: "https://play.google.com/store/apps/details?id=com.grtech.pakfit&hl=en&gl=US",
+  },
+  {
+    id: 2,
     title: "Todo List Web App",
     img: TodoWebApp_IMG,
     link: "https://todo-list-web-app-abdulbasit1993.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Calculator Web App",
     img: CalculatorWebApp_IMG,
     link: "https://abdulbasit1993.github.io/Calculator-WebApp/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Coming Soon",
     img: ComingSoon_IMG,
   },
@@ -45,6 +52,12 @@ export const webPortfolio = [
 export const mobilePortfolio = [
   {
     id: 1,
+    title: "PakFit",
+    img: PakFit_IMG,
+    link: "https://play.google.com/store/apps/details?id=com.grtech.pakfit&hl=en&gl=US",
+  },
+  {
+    id: 2,
     title: "Coming Soon",
     img: ComingSoon_IMG,
   },
